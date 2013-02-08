bf93953bb8e204516448a3bdb5c54a59c707cc3a
Hervé BREDIN <bredin@limsi.fr>
2013-02-08 14:20:21 +0100

    Working version of extract.py + activeLipInSync/inactiveLip region of interest

--- corpus/extract.py
+++ corpus/extract.py
@@ -1,52 +1,130 @@
+
+# random ID generator
+import string
+import random
+def id_generator(size=10, chars=string.ascii_lowercase):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+# segment cropper 
+from pyannote import Segment
+def crop_segment(segment, duration=3.):
+    middle = segment.middle
+    return Segment(middle-.5*duration, middle+.5*duration)
+
+# format seconds
+from datetime import timedelta
+def format_seconds(seconds):
+    td = timedelta(seconds=seconds)
+    days = td.days
+    seconds = td.seconds
+    microseconds = td.microseconds
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return '%02d:%02d:%02d.%03d' % (hours, minutes, seconds, microseconds/1000)
+
 from pyannote.parser import AnnotationParser, LSTParser
 from pyannote import Timeline
 
 # load speech turns groundtruth
-speaker = AnnotationParser().read('/people/bredin/Data/QCompere/phase1/train/groundtruth/speaker.mdtm')
+Speaker = AnnotationParser().read('/people/bredin/Data/QCompere/phase1/dev/groundtruth/speaker.mdtm')
 # load face tracks groundtruth
-head = AnnotationParser().read('/people/bredin/Data/QCompere/phase1/train/groundtruth/head.mdtm')
+Head = AnnotationParser().read('/people/bredin/Data/QCompere/phase1/dev/groundtruth/head.mdtm')
 # list of videos 
-uris = LSTParser().read('/people/bredin/Data/QCompere/phase1/train/lists/uri.LCP_PileEtFace.lst')
+uris = LSTParser().read('/people/bredin/Data/QCompere/phase1/dev/lists/uri.lst')
 
-for uri in uris[:1]:
+duration = 3.
+
+# ========================================================================
+# EXTRACT SEGMENTS WHERE THE UNIQUE FACE CORRESPONDS TO THE UNIQUE SPEAKER
+# ========================================================================
+
+f = open('/tmp/activeLipInSync.txt', 'w')
+
+for uri in uris:
     
-    oneSpeakerOneHeadMatch = Timeline(uri=uri)
-    oneSpeakerOneHeadMismatch = Timeline(uri=uri)
-    oneSpeakerTwoHeadsMatch = Timeline(uri=uri)
-    oneSpeakerTwoHeadsMismatch = Timeline(uri=uri)
+    print uri
     
-    s = speaker(uri)
-    h = head(uri)
+    speaker = Speaker(uri)
+    head = Head(uri)
     
-    # focus on segments were exactly one person is speaking
-    s = s >> s.timeline.segmentation()
-    timeline = Timeline([segment for segment in s 
-                                 if len(s.tracks(segment)) == 1], 
-                        uri=uri)
-    focus = timeline.coverage()
-    s = s.crop(focus, mode='intersection').smooth()
-    h = h.crop(focus, mode='intersection').smooth()
+    # timeline where there is exactly one speaker
+    tmp = speaker >> speaker.timeline.segmentation()
+    oneSpeakerTL = Timeline([s for s in tmp if len(tmp.tracks(s)) == 1],
+                          uri=uri)
     
-    # project both annotations on common timeline
-    timeline = (s.timeline + h.timeline).segmentation()
-    s = s >> timeline
-    h = h >> timeline
+    # timeline where there is exactly one head
+    tmp = head >> head.timeline.segmentation()
+    oneHeadTL = Timeline([s for s in tmp if len(tmp.tracks(s)) == 1],
+                          uri=uri)
     
-    for segment in timeline:
-        ls = s.get_labels(segment)
-        lh = h.get_labels(segment)
-        if len(ls) != 1:
-            raise ValueError('Overlapping speech!')
-        ls = ls.pop()
-        if len(lh) == 1:
-            lh = lh.pop()
-            if ls == lh:
-                oneSpeakerOneHeadMatch += segment
-            else:
-                oneSpeakerOneHeadMismatch += segment
-        elif len(lh) == 2:
-            if ls in lh:
-                oneSpeakerTwoHeadsMatch += segment
-            else:
-                oneSpeakerTwoHeadsMismatch += segment
-        
+    # focus on the intersection
+    oneSpeakerOneHeadTL = (oneSpeakerTL & oneHeadTL).segmentation()
+    
+    # timeline where speaker and head are the same
+    speaker = speaker >> oneSpeakerOneHeadTL
+    head = head >> oneSpeakerOneHeadTL
+    timeline = Timeline([s for s in oneSpeakerOneHeadTL 
+                           if speaker.get_labels(s) == head.get_labels(s)],
+                        uri=uri).coverage()
+    
+    # remove segments smaller than 3 seconds
+    # and extract the middle 3 seconds
+    timeline = Timeline([crop_segment(s, duration=duration) for s in timeline if s.duration > duration], uri=uri)
+    
+    speaker = speaker.crop(timeline, mode='intersection')
+    head = head.crop(timeline, mode='intersection')
+    
+    for s,t,l in speaker.iterlabels():
+        f.write('%s %s %s %s %s\n' % (uri, format_seconds(s.start),
+                                           format_seconds(s.duration), 
+                                           id_generator(), 
+                                           id_generator()))
+        f.flush()
+
+f.close()
+
+
+# =========================================================================
+# EXTRACT SEGMENTS WHERE THE UNIQUE FACE DOES NOT CORRESPOND TO ANY SPEAKER
+# =========================================================================
+
+f = open('/tmp/inactiveLip.txt', 'w')
+
+for uri in uris:
+    
+    print uri
+    
+    speaker = Speaker(uri)
+    head = Head(uri)
+    
+    # timeline where there is exactly one head
+    tmp = head >> head.timeline.segmentation()
+    oneHeadTL = Timeline([s for s in tmp if len(tmp.tracks(s)) == 1],
+                          uri=uri)
+    
+    anySpeakerOneHeadTL = (speaker.timeline & oneHeadTL).segmentation()
+    
+    
+    # timeline where unique head is not among speaker
+    speaker = speaker >> anySpeakerOneHeadTL
+    head = head >> anySpeakerOneHeadTL
+    timeline = Timeline([s for s in anySpeakerOneHeadTL 
+                           if head.get_labels(s).pop() not in speaker.get_labels(s)],
+                        uri=uri).coverage()
+    
+    # remove segments smaller than 3 seconds
+    # and extract the middle 3 seconds
+    timeline = Timeline([crop_segment(s, duration=duration) for s in timeline if s.duration > duration], uri=uri)
+    
+    speaker = speaker.crop(timeline, mode='intersection')
+    head = head.crop(timeline, mode='intersection')
+    
+    for s,t,l in speaker.iterlabels():
+        f.write('%s %s %s %s %s\n' % (uri, format_seconds(s.start),
+                                        format_seconds(s.duration), 
+                                        id_generator(),
+                                        id_generator()))
+        f.flush()
+
+f.close()
+
